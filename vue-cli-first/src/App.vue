d3e288b10bc141aa2d72339a65c0036dcda06db4
rota99C <rotaboolean@gmail.com>
2021-11-10 20:05:45 +0100

    creato componente con collegamento ad app.vue

--- vue-cli-first/src/App.vue
+++ vue-cli-first/src/App.vue
@@ -1,22 +1,30 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TitleVue/>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TitleVue from './components/TitleVue.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TitleVue
   }
 }
 </script>
 
 <style lang="scss">
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: 0;
+  }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
